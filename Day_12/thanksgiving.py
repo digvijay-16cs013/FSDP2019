84767b6be411d1666fb746b5e69b69b00c780aed
digvijay-16cs013 <ds93755@gmail.com>
2019-06-01 08:39:38 +0530

    Pandas and Matplotlib

---
+++ Day_12/thanksgiving.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 13:54:34 2019
+
+@author: Administrator
+"""
+
+import pandas as pd
+data = pd.read_csv('thanksgiving.csv', encoding = 'Windows 1252')
